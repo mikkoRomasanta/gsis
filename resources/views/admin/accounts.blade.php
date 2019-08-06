5420531f8f3aadb6b6c032d82702336f58cc06a3
Romasanta　JonathanMikko <17G500613>
2019-08-06 10:45:13 +0800

    Disable /register for USERs+GSADMIN && remove edit for GSADMIN @ accounts page

--- resources/views/admin/accounts.blade.php
+++ resources/views/admin/accounts.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="container h-100">
+        <input id="role" type="text" style="display:none" value="{{ Auth::user()->role }}" readonly>
         <table class="display compact table-striped" id="table-accounts">
             <thead>
                 <tr>
@@ -12,7 +13,11 @@
                     <th>Role</th>
                     <th>Last Password Change</th>
                     <th>Date Created</th>
-                    <th>Action</th>
+                    @if(Auth::user()->role == 'ADMIN')
+                        <th>Action</th>
+                    @else
+                        <th style='display:none'>Action</th>
+                    @endif
                 </tr>
             </thead>
         </table>
@@ -25,7 +30,6 @@
 
 @push('scripts')
     <script type="text/javascript">
-
         $(document).ready(function() {
 
             $.fn.dataTable.ext.buttons.register = {
@@ -35,38 +39,62 @@
                 }
             };
 
-            var tableUser = $('#table-accounts').DataTable({
-                ajax: {
-                    url: "{{route('get.user')}}",
-                    dataSrc: ''
-                    },
-                columns: [
-                    {'data' : 'id'},
-                    {'data' : 'username'},
-                    {'data' : 'name'},
-                    {'data' : 'status'},
-                    {'data' : 'role'},
-                    {'data' : 'updated_at'},
-                    {'data' : 'created_at'},
-                    { "defaultContent": "<a href='#' class='btn btn-info btn-sm' id='editBtn'>Edit</a>"}
-                ],
-                dom: 'Bfrtip',
-                buttons: [
-                            'register'
-                ]
-            });
+            var role = $('#role').val();
+            if(role === 'ADMIN'){
+                var tableUser = $('#table-accounts').DataTable({
+                    ajax: {
+                        url: "{{route('get.user')}}",
+                        dataSrc: ''
+                        },
+                    columns: [
+                        {'data' : 'id'},
+                        {'data' : 'username'},
+                        {'data' : 'name'},
+                        {'data' : 'status'},
+                        {'data' : 'role'},
+                        {'data' : 'updated_at'},
+                        {'data' : 'created_at'},
+                        { "defaultContent": "<a href='#' class='btn btn-info btn-sm' id='editBtn'>Edit</a>"}
+                    ],
+                    dom: 'Bfrtip',
+                    buttons: [
+                                'register'
+                    ]
+                });
 
-            $('#table-accounts tbody')
-		    .on( 'click', '#editBtn', function () {
-                $("#mdlEdit").modal();
-                var data = tableUser.row( $(this).parents('tr') ).data();
-                $('#nameBox').val(data.name);
-                $('#statusBox').val(data.status);
-                $('#roleBox').val(data.role);
-                $('#usernameBox').val(data.username);
-                $('#idBox').val(data.id);
-                $('#mdlTitle').html('EDIT | '+data.username);
-		});
+                $('#table-accounts tbody')
+                .on( 'click', '#editBtn', function () {
+                    $("#mdlEdit").modal();
+                    var data = tableUser.row( $(this).parents('tr') ).data();
+                    $('#nameBox').val(data.name);
+                    $('#statusBox').val(data.status);
+                    $('#roleBox').val(data.role);
+                    $('#usernameBox').val(data.username);
+                    $('#idBox').val(data.id);
+                    $('#mdlTitle').html('EDIT | '+data.username);
+            });
+        }
+        else{
+            $('#table-accounts').DataTable({
+                    ajax: {
+                        url: "{{route('get.user')}}",
+                        dataSrc: ''
+                        },
+                    columns: [
+                        {'data' : 'id'},
+                        {'data' : 'username'},
+                        {'data' : 'name'},
+                        {'data' : 'status'},
+                        {'data' : 'role'},
+                        {'data' : 'updated_at'},
+                        {'data' : 'created_at'}
+                    ],
+                    dom: 'Bfrtip',
+                    buttons: [
+                                'register'
+                    ]
+                });
+        }
 
         });
     </script>
