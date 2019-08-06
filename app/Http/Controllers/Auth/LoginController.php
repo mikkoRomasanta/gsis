8ec015fc1e229e397102933e88af361b57ac51b2
Romasanta　JonathanMikko <17G500613>
2019-08-06 10:59:06 +0800

    Block INACTIVE users from logging in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -20,18 +21,17 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
+
     protected $redirectTo = '/dashboard';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+    protected function credentials(Request $request) // Allow only active users
+    {
+        $credentials = $request->only($this->username(), 'password');
+        $credentials['status'] = 'ACTIVE';
+
+        return $credentials;
+    }
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
